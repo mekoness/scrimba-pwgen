ae13f02accf3238573b5ae79e7dbc5b4c37920c3
mekoness <mekoness@gmail.com>
2022-09-28 12:11:32 -0700

    Pre Checkboxes

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]
+const keys = {
+    characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    number: "0123456789",
+    symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
+  }
 let passOneEl = document.getElementById("password1")
 let passTwoEl = document.getElementById("password2")
 let pwLengthEl = document.getElementById("pw-length")
-let passOne = ""
-let passTwo = ""
+let inLettersEl = document.getElementById("in-letters-chk").checked
+let inSymbolsEl = document.getElementById("in-symbols-chk").checked
+let inNumbersEl = document.getElementById("in-numbers-chk").checked
 
-function getRandom() {
-    return characters[ Math.floor( Math.random() * characters.length ) ]
-}
+let getKey = [
+    function characters() {
+      return keys.characters[Math.floor(Math.random() * keys.characters.length)]
+    },
+    function number() {
+      return keys.number[Math.floor(Math.random() * keys.number.length)]
+    },
+    function symbol() {
+      return keys.symbol[Math.floor(Math.random() * keys.symbol.length)]
+    }
+]
 
 function getPasswords() {
     let passwordLength = pwLengthEl.value
-    for (let i = 0; i < passwordLength; i++) {
-        passOne += getRandom()
-    }
-    for (let i = 0; i < passwordLength; i++) {
-        passTwo += getRandom()
+    let passOne = ""
+    while (passwordLength > passOne.length) {
+      let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)]
+      passOne += keyToAdd()
     }
     passOneEl.textContent = passOne
-    passTwoEl.textContent = passTwo
-    passOne = ""
-    passTwo = ""
 }
