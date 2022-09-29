a2d654233419991c363475b907872d8cf39c983d
mekoness <mekoness@gmail.com>
2022-09-29 15:08:53 -0700

    If statements

--- index.js
+++ index.js
@@ -7,24 +7,37 @@ let passwordEl = document.getElementById("password")
 let pwLengthEl = document.getElementById("pw-length")
 
 let letters = document.getElementById("letters").checked
-let symbols = document.getElementById("symbols").checked
 let numbers = document.getElementById("numbers").checked
+let symbols = document.getElementById("symbols").checked
 
 let getKey = [
     function characters() {
-      return keys.characters[Math.floor(Math.random() * keys.characters.length)]
+      if (letters === true) {
+        return keys.characters[Math.floor(Math.random() * keys.characters.length)]
+      } else {
+        return ""
+      }
     },
     function number() {
-      return keys.number[Math.floor(Math.random() * keys.number.length)]
+      if (numbers === true) {
+        return keys.number[Math.floor(Math.random() * keys.number.length)]
+      } else {
+        return ""
+      }
     },
     function symbol() {
-      return keys.symbol[Math.floor(Math.random() * keys.symbol.length)]
+      if (symbols === true) {
+        return keys.symbol[Math.floor(Math.random() * keys.symbol.length)]
+      } else {
+        return ""
+      }
     }
 ]
 
 function getPassword() {
     let passwordLength = pwLengthEl.value
     let password = ""
+
     while (passwordLength > password.length) {
       let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)]
       password += keyToAdd()
@@ -37,4 +50,8 @@ function copyPassword() {
     password = passwordEl.textContent
     navigator.clipboard.writeText(password)
     alert("Copied password.")
+}
+
+function checkbox() {
+  console.log(symbols)
 }
